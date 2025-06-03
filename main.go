fb9831d1e6576b0363e4a603fd6707739ba3f30a
AxelFlores05 <axelflores05@gmail.com>
2025-06-02 21:17:58 -0600

    CORS añadidos para la conexión de front y back

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"countries_backend/handler"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	
 )
 
@@ -12,6 +13,9 @@ func main() {
 
 	// Rutas
 	e.GET("/countries", handler.GetCountries)
+
+	//CORS
+	e.Use(middleware.CORS())
 
 	// Lenvantamos el servidor
 	e.Logger.Fatal(e.Start(":8080"))
